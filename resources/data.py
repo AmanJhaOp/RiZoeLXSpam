a8b09982546b2fdf051cfcd6aa2292a2f05df025
#Shreya_Op ❤✨ <97891442+AmanJhaOp@users.noreply.github.com>
2022-03-01 18:01:51 +0530

    Update data.py

--- resources/data.py
+++ resources/data.py
@@ -7,9 +7,4 @@ RAID = [
     "It does'not matter how much time had you blocked me , I still Love you ! 🥰❤️❣️❣️",
     "I love you Shreyuuu! Meli Jann",
     "I love you a lot❣️❣️",
-    
-]
-
-RiZoeLX = [1517994352, 2086101519, 136817688]
-
 
